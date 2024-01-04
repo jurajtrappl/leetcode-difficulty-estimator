7840faadc90bf68f5723f5425f92d34372a834f5
filipmihal <fifomihal@gmail.com>
2024-01-04 21:27:39 +0100

    top pipeline

--- sklearn_pipeline.py
+++ sklearn_pipeline.py
@@ -16,23 +16,36 @@ import sklearn.svm
 import csv
 import sklearn.model_selection
 
+from collections import Counter
+import numpy as np
+from sklearn.model_selection import train_test_split
+from bs4 import BeautifulSoup
+import json
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+
+
 from sklearn.metrics import f1_score
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
-parser.add_argument("--c_n", default=3, type=int, help="Character n-grams")
+parser.add_argument("--c_n", default=5, type=int, help="Character n-grams")
 parser.add_argument("--c_l", default=False, action="store_true", help="Character lowercase")
 parser.add_argument("--c_tf", default="binary", type=str, help="Character TF type")
 parser.add_argument("--c_mf", default=None, type=int, help="Character max features")
 parser.add_argument("--c_wb", default=False, action="store_true", help="Character wb")
 parser.add_argument("--model", default="lsvm", type=str, help="Model type")
-parser.add_argument("--w_n", default=2, type=int, help="Word n-grams")
+parser.add_argument("--w_n", default=3, type=int, help="Word n-grams")
 parser.add_argument("--w_l", default=False, action="store_true", help="Word lowercase")
 parser.add_argument("--w_tf", default="log", type=str, help="Word TF type")
 parser.add_argument("--w_mf", default=None, type=int, help="Word max features")
-parser.add_argument("--hidden_layer", default=1024, type=int, help="Hidden layer size")
+parser.add_argument("--hidden_layer", default=16, type=int, help="Hidden layer size")
 parser.add_argument("--alpha", default=0.0005, type=float, help="Alpha for L2 regularization")
 parser.add_argument("--activation", default="relu", type=str, help="Activation function")
 def convert_dfficulty_class(name):
@@ -49,17 +62,66 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
     np.random.seed(args.seed)
     
 
+    # 1. Load the dataset.
+    with open('leetcode_problems_dataset.json', 'r') as f:
+        problems = json.load(f)
+        
+    # 2. Remove HTML tags.
     problems_without_html = []
-    with open('problems_without_html.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in reader:
-            problems_without_html.append(row)
-    
-    problems = [problem[0] for problem in problems_without_html]
-    difficulty_classes = [convert_dfficulty_class(problem[1]) for problem in problems_without_html]
-    
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(problems, difficulty_classes, random_state=args.seed, stratify=difficulty_classes, test_size=0.2)
+    for problem_name, problem_data in tqdm(problems.items()):
+        if not problem_data["content"]:
+            continue
+        
+        problems_without_html.append((BeautifulSoup(problem_data["content"], "html.parser").get_text(), problem_data["difficulty"]))
+        
+    # 3. Create X, y
+    X, y = [], []
+    difficulties_int = { "Easy": 0, "Medium": 1, "Hard": 2 }
+    for problem_description, difficulty in problems_without_html:
+        X.append(problem_description)
+        y.append(difficulties_int[difficulty])
+        
+    # 4. Prepare downsampling function.
+    def downsample_dataset(features, labels):
+        """
+        Downsamples the dataset to have an equal distribution of classes.
+
+        Parameters:
+        X (list): Feature data.
+        y (list): Corresponding labels.
+
+        Returns:
+        tuple: Downsampled feature data and labels.
+        """
+        paired_data = list(zip(features, labels))
+        class_distribution = Counter(labels)
+
+        min_samples = min(class_distribution.values())
+
+        downsampled_data = []
+        class_counts = {cls: 0 for cls in class_distribution.keys()}
+
+        for data, label in paired_data:
+            if class_counts[label] < min_samples:
+                downsampled_data.append((data, label))
+                class_counts[label] += 1
+
+        features_downsampled, labels_downsampled = zip(*downsampled_data)
+
+        return list(features_downsampled), list(labels_downsampled)
+
+    # 5. Downsample dataset.
+    X, y = downsample_dataset(X, y)
+
+    # 6. (Optional)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=args.seed, stratify=y)
+    X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, test_size=0.5, random_state=args.seed, stratify=y_val)
+
+    X_train, X_val, X_test, y_train, y_val, y_test = np.array(X_train), np.array(X_val), np.array(X_test), np.array(y_train), np.array(y_val), np.array(y_test)
+
+    # combine train and val
+    X_train = np.concatenate((X_train, X_val))
+    y_train = np.concatenate((y_train, y_val))
 
     model = sklearn.pipeline.Pipeline([
         ("feature_extraction",
@@ -73,8 +135,8 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
                     binary=args.c_tf == "binary", sublinear_tf=args.c_tf == "log", max_features=args.c_mf)),
             ] if args.c_n else []))),
         ("estimator", {
-            "perc": sklearn.linear_model.Perceptron(tol=1e-6, early_stopping=True, validation_fraction=0.1, verbose=1, penalty="l2", random_state=args.seed),
-            "mlp_c": sklearn.neural_network.MLPClassifier(hidden_layer_sizes=args.hidden_layer, max_iter=7, verbose=1, alpha=args.alpha, early_stopping=True, activation=args.activation),
+            "perceptron": sklearn.linear_model.Perceptron(tol=1e-6, early_stopping=True, validation_fraction=0.1, verbose=1, penalty="l2", random_state=args.seed),
+            "mlp_c": sklearn.neural_network.MLPClassifier(hidden_layer_sizes=args.hidden_layer, max_iter=100, verbose=1, alpha=args.alpha, early_stopping=True, activation=args.activation),
             "mlp_r": sklearn.neural_network.MLPRegressor(hidden_layer_sizes=args.hidden_layer, max_iter=100, verbose=1, alpha=args.alpha, early_stopping=True, activation=args.activation),
             "svm": sklearn.svm.SVC(verbose=1, random_state=args.seed),
             "lsvm": sklearn.svm.LinearSVC(verbose=1, random_state=args.seed),
@@ -86,9 +148,24 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
     print('Test accuracy:')
     print(model.score(X_test, y_test))
 
+    predictions = model.predict(X_test)
+
     print('Test F1 score:')
-    print(f1_score(y_test, model.predict(X_test), average='macro'))
-    print(f1_score(y_test, model.predict(X_test), average='micro'))
+    print(f1_score(y_test, predictions, average='macro'))
+    print(f1_score(y_test, predictions, average='micro'))
+
+
+    cm = confusion_matrix(y_test, predictions)
+
+    plt.figure(figsize=(12, 10))
+
+    class_labels = ['Easy', 'Medium', 'Hard']
+    sns.heatmap(cm, annot=True, fmt='d', cmap='BuGn', xticklabels=class_labels, yticklabels=class_labels)
+    plt.title(f'Confusion Matrix for {args.model}')
+    plt.ylabel('True Label')
+    plt.xlabel('Predicted Label')
+    plt.savefig(f"{args.model}.png", dpi=300)
+    plt.show()
 
 
     if args.model == "mlp_r":
@@ -100,8 +177,8 @@ def main(args: argparse.Namespace) -> Optional[npt.ArrayLike]:
         model_name = f"{args.model}_{args.w_n}_{args.c_n}"
     else:
         model_name = f"{args.model}_{args.w_n}_{args.c_n}_{args.hidden_layer}_{args.alpha}_{args.activation}"
-    with lzma.open(f"models/{model_name}.pickle", "wb") as model_file:
-        pickle.dump(model, model_file)
+    # with lzma.open(f"models/{model_name}.pickle", "wb") as model_file:
+    #     pickle.dump(model, model_file)
     print(f"Model saved to models/{model_name}.pickle")
 
 if __name__ == "__main__":
